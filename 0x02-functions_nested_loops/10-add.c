2a139d5eedc49f657289248f0e015016084b9072
amira-creator <amira.abdelfattah99@gmial.com>
2023-04-25 16:59:46 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two interger and returns result
+ * @x: number begin add
+ * @y: number begin add
+ * Return: Always 0
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
