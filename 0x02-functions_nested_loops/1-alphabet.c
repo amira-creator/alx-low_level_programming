a60b5f83a925de6c180fdd6c4cd76ddb8265ce64
amira-creator <amira.abdelfattah99@gmial.com>
2023-04-26 11:11:35 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * main - Entery point
- * print_alphabet - using putchar prototype
+ * print_alphabet - Entry point
  * Return: 0
  */
 void print_alphabet(void)
 {
-        char c = 'a';
+	char c = 'a';
 
-        while (c <= 'z')
-        {
-                _putchar(c);
-                c++;
-        }
-        _putchar('\n');
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 }
