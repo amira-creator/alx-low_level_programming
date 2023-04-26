e887ae4fb6dea1008d8031ef0274d6f2fd62e2fe
amira-creator <amira.abdelfattah99@gmial.com>
2023-04-26 15:17:21 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,58 @@
 #include <stdio.h>
 /**
+ * numLength - return the length of string
+ * @num: operate  number
+ * Return: number of digit
+ */
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+	return (length);
+}
+
+/**
  * main - main block
- * Description: computed and prints the sum of all the multiples of 3 or
- * 5 below 1024 (excluded), followed by a new line
+ * Description: print the first 98 fibonacci numbers
+ * starting with 1 and 2 followed by a new line
  * Return: 0
  */
 int main(void)
 {
-	int i = 0;
-	unsigned long int a = 0, b = 1, next = 0;
+	int count, i;
+	unsigned long int f1 = 1, f2 = 2;
+	unsigned long int sum, mx = 100000000, flo = 0, f2o = 0, sumo = 0;
 
-	while (i < 98)
+	for (count = 1; count <= 98; count++)
 	{
-		next = a + b;
-		a = b;
-		b = next;
-		printf("%lu", next);
+		if (flo > 0)
+			printf("%lu", flo);
+		i = numLength(mx) - 1 - numLength(f1);
 
-		if (i < 97)
+		while (flo > 0 && i > 0)
+		{
+			printf("%d", 0);
+			i--;
+		}
+		printf("%lu", f1);
+
+		sum = (f1 + f2) % mx;
+		sumo = flo + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		flo = f2o;
+		f2 = sum;
+		f2o = sumo;
+		if (count != 98)
 			printf(", ");
-		i++;
+		else
+			printf("\n");
 	}
-	_putchar('\n');
 	return (0);
 }
