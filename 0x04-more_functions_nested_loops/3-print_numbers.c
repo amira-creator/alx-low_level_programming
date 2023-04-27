d67bac0653161889301462300789c7b183fb36b9
amira-creator <amira.abdelfattah99@gmial.com>
2023-04-27 04:33:02 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 /**
  * print_numbers - print 0 - 9
  *          only using _putchar twice
- *          Return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 void print_numbers(void)
 {
 	int num = 0;
 
 	do {
-		_puctchar(num + 48);
+		_putchar(num + 48);
 		num++;
 	} while (num >= 0 && num <= 9);
 	_putchar('\n');
