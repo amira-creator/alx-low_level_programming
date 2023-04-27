32652373bf4a3a9f815f52ebe852ea63e8704dec
amira-creator <amira.abdelfattah99@gmial.com>
2023-04-27 05:51:04 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal line
+ * @n: is the number of time the \ character should be printed
+ */
+void print_diagonal(int n)
+{
+	int postn, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= n; postn++)
+		{
+			for (space = 1; space <= postn; space++)
+				_putchar(' ');
+			_putchar(92); /* is equal to '/' char*/
+			_putchar('\n');
+		}
+	}
+}
