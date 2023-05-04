2d6a14002d4b0378c8803340f30a01131559ff41
amira-creator <amira.abdelfattah99@gmial.com>
2023-05-04 12:49:24 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 
 /**
  * cap_string - function capitalizes most of the  words in  string
@@ -7,30 +9,23 @@
  */
 char *cap_string(char *s)
 {
-	int i, j;
-	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int index = 0;
 
-	i = 0;
-	while (*(s + i) != '\0')
+	while (s[index])
 	{
-		if (*(s + i) >= 'a' && *(s + i) >= 'z')
-		{
-			if (i == 0)
-			{
-				*(s + i) = *(s + i) - 32;
-			}
-			else
-			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a[j] == *(s + i - 1))
-					{
-						*(s + i) = *(s + i) - 32;
-					}
-				}
-			}
-		}
-		i++;
+		while (!(s[index] >= 'a' && s[index] <= 'z'))
+			index++;
+
+		if (s[index - 1] == ' ' || s[index - 1] == '\t' ||
+		    s[index - 1] == '\n' || s[index - 1] == ',' ||
+		    s[index - 1] == ';' || s[index - 1] == '.' ||
+		    s[index - 1] == '!' || s[index - 1] == '?' ||
+		    s[index - 1] == '"' || s[index - 1] == '(' ||
+		    s[index - 1] == ')' || s[index - 1] == '{' ||
+		    s[index - 1] == '}' || index == 0)
+			s[index] -= 32;
+
+		index++;
 	}
 	return (s);
 }
