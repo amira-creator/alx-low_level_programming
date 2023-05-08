47370f3e67fd322d7092b79affd0043ad103af37
amira-creator <amira.abdelfattah99@gmail.com>
2023-05-08 07:36:32 -0700

    101-crackme_password

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,19 @@
 #include "main.h"
- /**
-  *_memcpy - a function that copies a memory area
-  *@dest: memory area
-  *@src: memory where is copied
-  *@n: number of bytes
-  *Return: copied memory with n byted changed
-  */
+
+/**
+ * *_memcpy -  a function that copies memory area
+ * @dest: memory where is stored
+ * @src: memory where is copied
+ * @n: number of bytes
+ * Return: copied memory with n byted changed
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int r = 0;
-	int i = n;
+	unsigned int r;
 
-	for (; r < i; r++)
+	for (r = 0; r < n; r++)
 	{
 		dest[r] = src[r];
-		n--;
 	}
 	return (dest);
 }
