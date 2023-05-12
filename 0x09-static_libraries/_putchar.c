335e30b4a40fac810b9d1e48f721edb1b53c0e23
amira-creator <amira.abdelfattah99@gmail.com>
2023-05-12 05:09:41 -0700

    libmy.a

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - writes th echaracter c to stdout
+ * @c: the caharacter to print
+ * Return: on success 1
+ * on error , -1 is reaturned, and errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
