f85a4e99179e4276b65fe3024f820486ec4d4f80
amira-creator <amira.abdelfattah99@gmail.com>
2023-05-18 04:29:41 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,6 +15,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
 		p = malloc(new_size);
 		if (p == NULL)
 			return (NULL);
