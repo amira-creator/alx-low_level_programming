1499012a4f87313d6d99fd81a4c7761d477c5fe7
amira-creator <amira.abdelfattah99@gmail.com>
2023-05-24 08:24:30 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes function given as parameter on each element array
+ * @array: the int array
+ * @size: the array size
+ * @action: function pointer
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int *end = array + size - 1;
+
+	if (array && size && action)
+		while (array <= end)
+			action(*array++);
+}
