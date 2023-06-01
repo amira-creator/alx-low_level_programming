75906308bce4602b89401661dd053fc8d3841b11
amira-creator <amira.abdelfattah99@gmail.com>
2023-06-01 03:16:10 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef LISTS_H
-#define LISTs_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,5 +23,6 @@ int _putchar(char c);
 int _strlen(char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
