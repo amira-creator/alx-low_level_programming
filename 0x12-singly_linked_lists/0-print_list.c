0d8c4d0457b31df7f71ed41175e8a5955978de9a
amira-creator <amira.abdelfattah99@gmail.com>
2023-06-01 04:11:17 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t s = 0;
 
 	while (h)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u]%s\n", h->len, h->str);
 		h = h->next;
-		i++;
+		s++;
 	}
 
-	return (i);
+	return (s);
 }
